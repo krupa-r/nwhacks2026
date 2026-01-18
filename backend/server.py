59faef01899aef0f645b9f82b8e13517b641b3aa
brodrigues2005 <rodriguesbrett85@gmail.com>
2026-01-17 16:15:05 -0800

    implemented openai api

--- backend/server.py
+++ backend/server.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+
+from fastapi import FastAPI, Request
+from models import OpenAiResponse
 
 app = FastAPI()
 
@@ -10,3 +12,11 @@ app = FastAPI()
 @app.get("/")
 def testing():
     return {"name": "Henry"}
+
+@app.post("/openaiAPItest")
+async def openai(request : Request):
+    body = await request.json()
+    user_input = body["userInput"]
+
+    return OpenAiResponse(userInput = user_input)
+    
